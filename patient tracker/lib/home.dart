b10ad437f8338f27fcf12aff849024dcb7b6bf4c
Mayuka Nozaki <Noza3mayu3@mail.fresnostate.edu>
2024-02-16 22:20:11 -0800

    fix a part of color

--- patient tracker/lib/home.dart
+++ patient tracker/lib/home.dart
@@ -8,6 +8,9 @@ class Home extends StatelessWidget {
     return Scaffold(        
         appBar: AppBar(
           centerTitle: true,
+          leading: BackButton(
+            color: Colors.white    // change back button color
+          ),
           title: const Text(
             "Patient Tracker",
             style: TextStyle(
@@ -24,13 +27,18 @@ class Home extends StatelessWidget {
         body: Scaffold(
           backgroundColor: Colors.orange[50],
           body: Center(
-            child: Text('Hello'),
+            child: Text('Hello :)'),
           ),
         ),    
         bottomNavigationBar: BottomNavigationBar(
           backgroundColor: Color(0xFF323264),
+          selectedItemColor: Color.fromARGB(255, 124, 190, 240),
+          unselectedItemColor: Colors.white,  // set icon color when unselected
           items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Tab1'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home), 
+              label: 'Tab1',
+              ),
             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Tab2'),
             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Tab3'),
           ],
